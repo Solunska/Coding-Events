4d4ee1b063e4250f34d4c467540854f58ddad6d0
solunska <a_solunska@outlook.com>
2024-10-27 19:25:13 +0100

    navbar started

--- src/components/MainNavigation.tsx
+++ src/components/MainNavigation.tsx
@@ -1,12 +1,19 @@
-export default function MainNavigaion(){
-    return <nav>
-        <ul>
-            <li>Home</li>
-            <li>Events</li>
-            <li>Blog</li>
-            <li>Community</li>
-            <li>About</li>
-            <li>Contact</li>
+import classes from './MainNavigation.module.css'
+
+export default function MainNavigaion() {
+    return <nav className={classes.navbar}>
+        <ul className={classes.itemsContainer}>
+            <li>
+                <a>Code Ville</a>
+            </li>
+            <li>
+                <a>Home</a>
+                <a>Events</a>
+                <a>Blog</a>
+                <a>Community</a>
+                <a>About</a>
+                <a>Contact</a>
+            </li>
         </ul>
     </nav>
 }
